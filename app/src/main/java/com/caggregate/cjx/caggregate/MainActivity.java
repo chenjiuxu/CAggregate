6df2744970738483809275c70ee130bf3c4f4d1e
chenjiuxu <1570595476@qq.com>
2017-05-21 13:34:10 +0800

    项目开始

---
+++ app/src/main/java/com/caggregate/cjx/caggregate/MainActivity.java
@@ -0,0 +1,13 @@
+package com.caggregate.cjx.caggregate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
